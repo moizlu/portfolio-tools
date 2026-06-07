18f1aaf01c045a8e0b6f4c08f6931de7bf9da60d
もいずる <momizibe@outlook.jp>
2026-06-07 16:35:31 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,10 @@
     import { m } from "$lib/paraglide/messages";
 </script>
 
+<svelte:head>
+    <title>{m.tools()} | moizlu</title>
+</svelte:head>
+
 <main class="w-60 mx-auto flex flex-col justify-center items-center text-xl font-bold gap-3 text-center">
     <a title={m.pomodoro_timer()} href={resolve("/pomodoro")} class="w-full p-2 button-general button-label">{m.pomodoro_timer()}</a>
     <a title={m.pomodoro_timer()} href={resolve("/counter")} class="w-full p-2 button-general button-label">{m.counter()}</a>
